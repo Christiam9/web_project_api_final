1da2b1d819bc433db885831a44db5c8e24b61c9e
ChristiamR <christiancho0.9@gmail.com>
2026-07-07 00:01:22 -0500

    Finaliza la etapa 1.1 del frontend con React Router, diseño responsive y mejoras en la interfaz

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,4 @@
 import Header from "../Header/Header";
-import About from "../About/About";
-import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./vendor/variables.css";
+import "./vendor/fonts.css";
 import "./index.css";
 import App from "./components/App/App.jsx";
 
